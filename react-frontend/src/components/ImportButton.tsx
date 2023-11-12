f866dcfee5b6b538fdadcc7a11ac97e40120bb19
Will Brodowski <wbrodow@gmail.com>
2023-11-12 17:32:05 -0600

    Import button converting csv to json

--- react-frontend/src/components/ImportButton.tsx
+++ react-frontend/src/components/ImportButton.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import csvtojson from 'csvtojson';
+import { async } from "q";
 
-function Submit_Button() {
+function Import_Button() {
     return(
         <div style= {{textAlign:"center"}}>
             <h1>Slice</h1>
-            <form>
-                <input type={"file"} accept={".csv"} />
-                <button>Import CSV Budget Sheet</button>
+            <form id ="input-button" >
+                <input type={"file"} accept={".csv"}/>
+                <input type = "submit" value = 'Upload'/>
+                <button type='submit'>Import CSV Budget Sheet</button>
             </form>
         </div>
     );
 }
+
+const convertCsvToJson = async (csvFile) => {
+    const jsonArray = await csvtojson().fromFile(csvFile.path);
+    return jsonArray;
+};
+
+const sendJsonToApi = async(jsonData) =>{
+    try {
+        const response = await axios.post('API Endpoint',jsonData,{
+            headers:{
+                'Content-Type':'application/json',
+            },
+            
+        });
+        console.log(response.data);
+    }catch (error){
+        console.error('Error sending data to API:',error);
+    }
+};
+
+
+
